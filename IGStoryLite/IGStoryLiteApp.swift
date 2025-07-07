89f9b283639b8ba3c48be44b22caa3744c94d896
Robert Tanase <robert.tanase97@gmail.com>
2025-07-07 08:13:22 +0300

    Initial Commit

---
+++ IGStoryLite/IGStoryLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGStoryLiteApp.swift
+//  IGStoryLite
+//
+//  Created by TCode on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct IGStoryLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
