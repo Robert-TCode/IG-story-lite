c29b39eac3a8055902ae5a0455594a3429a9a519
Robert Tanase <robert.tanase97@gmail.com>
2025-07-07 10:44:34 +0300

    Add StoryUIModel

---
+++ IGStoryLite/Features/Stories/Presentation/Story/ViewModel/StoryUIModel.swift
@@ -0,0 +1,19 @@
+//
+//  StoryUIModel.swift
+//  IGStoryLite
+//
+//  Created by TCode on 7/7/25.
+//
+
+import Foundation
+
+struct StoryUIModel {
+    var id: String
+    var message: String
+    var creationDate: String
+    var imageURL: URL
+    var userImageURL: URL
+    var username: String
+    var isLiked: Bool
+    var wasSeen: Bool
+}
